7bea52cc1b9ce24ffd9843561b8221749208b0f0
HmerMza <hkooze7@gmail.com>
2023-07-31 21:33:40 -0500

    preparandolo para produccion

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var mongoose = require("mongoose");
+var app = require("./app");
+var port = process.env.PORT || 3900;
+
+mongoose.set("useFindAndModify", false);
+mongoose.Promise = global.Promise;
+mongoose
+  .connect("mongodb+srv://heymer:sistemas@prueba.yud83dm.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexión a la base de datos correcta !!!");
+
+    // Crear servidor y ponerme a escuchar peticiones HTTP
+
+    app.listen(port, () => {
+      console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
+  });
